6303bc76fb925cedfb5231c848251638b1d0df0a
www8899878 <www8899878@163.com>
2020-06-05 22:03:08 +0800

    driver

--- python/zidonghua/selenium/web/webbd.py
+++ python/zidonghua/selenium/web/webbd.py
@@ -3,14 +3,14 @@
 from selenium import webdriver
 #引入包
 
-browser = webdriver.Chrome(executable_path="/home/selenium/chromedriver")
+driver = webdriver.Chrome(executable_path="/home/selenium/chromedriver")
 #引入浏览器驱动
-browser.get("http://www.baidu.com")
+driver.get("http://www.baidu.com")
 #输入网址
-browser.find_element_by_id("kw").send_keys("selenium")
+driver.find_element_by_id("kw").send_keys("selenium")
 #找到元素 输入值
-browser.find_element_by_id("su").click()
+driver.find_element_by_id("su").click()
 #找到元素 点击操作
-browser.save_screenshot('baidu.png')
-browser.quit()
+driver.save_screenshot('baidu.png')
+driver.quit()
 # 关闭浏览器
