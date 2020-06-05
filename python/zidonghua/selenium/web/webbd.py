ff32bf68dda123120865cd73de841975b19815d3
www8899878 <www8899878@163.com>
2020-06-05 12:53:58 +0800

    123

--- python/zidonghua/selenium/web/webbd.py
+++ python/zidonghua/selenium/web/webbd.py
@@ -1,11 +1,16 @@
 # coding = utf-8
 
 from selenium import webdriver
-
+#引入包
 
 browser = webdriver.Chrome(executable_path="/home/selenium/chromedriver")
-
+#引入浏览器驱动
 browser.get("http://www.baidu.com")
+#输入网址
 browser.find_element_by_id("kw").send_keys("selenium")
+#找到元素 输入值
 browser.find_element_by_id("su").click()
+#找到元素 点击操作
+browser.save_scrrrnshot('baidu.png')
 browser.quit()
+# 关闭浏览器
