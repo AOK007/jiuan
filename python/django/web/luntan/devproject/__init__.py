71a59a367094897fc71c1e1dc75ff200267bc799
www8899878 <www8899878@163.com>
2020-06-09 15:47:12 +0800

    blog

---
+++ python/django/web/luntan/devproject/__init__.py
@@ -0,0 +1,5 @@
+import pymysql
+from pexpect.replwrap import python
+
+# python.version_info=(1,3,13,"final",0)
+pymysql.install_as_MySQLdb()
--- python/django/web/luntan/stray/__init__.py
+++ python/django/web/luntan/stray/__init__.py
@@ -1,3 +1,5 @@
-# import pymysql
+import pymysql
+from pexpect.replwrap import python
+
 # python.version_info=(1,3,13,"final",0)
-# pymysql.install_as_MySQLdb()
+pymysql.install_as_MySQLdb()
