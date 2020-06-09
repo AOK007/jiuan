6b47bb20f140574166fb609cf3607e0bd41900cf
www8899878 <www8899878@163.com>
2020-06-09 16:55:08 +0800

    ls

--- python/django/web/luntan/devproject/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DevprojectConfig(AppConfig):
-    name = 'devproject'
--- python/django/web/luntan/stray/__init__.py
+++
@@ -1,5 +0,0 @@
-import pymysql
-from pexpect.replwrap import python
-
-# python.version_info=(1,3,13,"final",0)
-pymysql.install_as_MySQLdb()
