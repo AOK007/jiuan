b5a282a7f8428c807a714f180fd91e93033e8719
www8899878 <www8899878@163.com>
2020-09-01 22:24:04 +0800

    baojia

--- python/pandas/baojia.py
+++ python/pandas/baojia.py
@@ -1,8 +1,20 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
-import numpd as np
+import numpy as np
 
-df = pd.DataFrame({
-    ['tengfei','yunct'],[10000,6000],[1,1]
+df1 = pd.DataFrame([
+    [ "腾飞系统",10000, 1],
+    ["云餐厅", 6000, 1],
+    [ "公有云",6000, 1],
+    [ "餐具",7, 1200] ],
+    columns=["型号","单价", "数量"])
+#index=('tengfei','yuncant','gyy','canju'))
+df1['价格']= df1['单价'] * df1['数量']
+df1["未税"]=df1['价格'].sum()
+#df2=df1["未税"]
 
-})
-df
+df1['含税']=df1["未税"]*1.13
+#df3=df1['含税']
+#df3=df1['含税']
+#df1.index['未税']= df1['价格']
+print(df1)
