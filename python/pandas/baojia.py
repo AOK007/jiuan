400087ea05b9bbf3ce5c9655f9859c34d32a5612
www8899878 <www8899878@163.com>
2020-09-02 21:00:50 +0800

    1236

--- python/pandas/baojia.py
+++ python/pandas/baojia.py
@@ -10,11 +10,18 @@ df1 = pd.DataFrame([
     columns=["型号","单价", "数量"])
 #index=('tengfei','yuncant','gyy','canju'))
 df1['价格']= df1['单价'] * df1['数量']
-df1["未税"]=df1['价格'].sum()
-#df2=df1["未税"]
-
-df1['含税']=df1["未税"]*1.13
+# df1["未税"]=df1['价格'].sum()
+# #df2=df1["未税"]
+#
+# df1['含税']=df1["未税"]*1.13
 #df3=df1['含税']
 #df3=df1['含税']
 #df1.index['未税']= df1['价格']
+
 print(df1)
+
+df2 = pd.DataFrame(
+    {df1['价格'].sum(),df1['价格'].sum()*1.13},
+    index=("未税","含税")
+)
+print(df2)
