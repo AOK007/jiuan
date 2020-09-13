82f29e34752305ebd1403f80021d680d3da3344e
www8899878 <www8899878@163.com>
2020-09-13 11:40:04 +0800

    99

---
+++ c/99x.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+     int n;
+     scanf("%d",&n);
+     n=9;
+     int i,j;
+     i =1;
+    while ( i<=n ) 
+    {
+        j = 1;
+        while ( j <= i  )
+        {
+            printf("%d*%d=%d",j,i,i*j);
+            if ( j*i < 10){
+                printf("   ");
+            }else
+            {
+                printf("   ");
+            }
+            j++;
+        }
+        i++;
+
+    }
+        return 0;   
+
+
+}
