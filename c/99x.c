92614cda6dd95af696dd585d81ec700fcecf19a8
www8899878 <www8899878@163.com>
2020-09-14 21:35:41 +0800

    1062

--- c/99x.c
+++ c/99x.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-     int n;
+     int  n;
      scanf("%d",&n);
      n=9;
      int i,j;
-     i =1;
+     i=1;
     while ( i<=n ) 
     {
-        j = 1;
+        j=1;
         while ( j <= i  )
         {
             printf("%d*%d=%d",j,i,i*j);
-            if ( j*i < 10){
+            if ( j*i <10){
                 printf("   ");
             }else
             {
